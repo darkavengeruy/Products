e0590068f34f816c9fe56d3ed44e8164a2d87ae9
FERNANDO FRANCO <ferfranco.uy@hotmail.com>
2017-11-27 01:10:28 -0300

    Basic projects

---
+++ Products.Backend/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Entity;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace Products.Backend
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            Database.SetInitializer(
+                new MigrateDatabaseToLatestVersion<Models.DataContextLocal, 
+                Migrations.Configuration>());
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
---
+++ Products.Domain/DataContext.cs
@@ -0,0 +1,12 @@
+﻿namespace Products.Domain
+{
+    using System.Data.Entity;
+    public class DataContext: DbContext
+    {
+        public DataContext() : base("DefaultConnection") 
+        {           
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
